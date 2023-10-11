49a1e4f12d5d22311249b22cf2404d66314cc37b
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-11 03:25:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Class_Methods
+{
+    internal class Student
+    {
+     public double weight;
+     public string name;
+     public int age;
+
+            public Student(double aWeight, string aName, int aAge)
+        {
+            this.weight = aWeight;// the word is an explict way
+            this.name = aName;
+            this.age = aAge;    
+        }
+
+        //you now have one stop shop to test Obesity of your student
+        public bool IsObese()
+        {
+            if (weight > 60)
+            {
+                return true;
+            }
+            else return false;
+        }
+        }
+    }
+
